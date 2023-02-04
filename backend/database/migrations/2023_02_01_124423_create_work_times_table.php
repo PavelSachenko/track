884df5db4ed1222726fa92704eda1057bd8fa6a5
Pavlo <pavel31.pavel31@gmail.com>
2023-02-04 13:28:58 +0200

    fix migrations

--- backend/database/migrations/2023_02_01_124423_create_work_times_table.php
+++ backend/database/migrations/2023_02_01_124423_create_work_times_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,10 +15,10 @@ return new class extends Migration
         Schema::create('work_times', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('current_mode', 30);
-            $table->json('custom_times');
-            $table->json('every_day_times');
-            $table->json('weekdays_times');
+            $table->string('current_mode', 30)->default('custom');
+            $table->json('custom_times')->default('[{"day":1,"from":"08:00","to":"16:00"},{"day":2,"from":"08:00","to":"16:00"},{"day":3,"from":"08:00","to":"19:00"},{"day":4,"from":"08:00","to":"16:00"},{"day":5,"from":"08:00","to":"16:00"},{"day":6,"from":"08:00","to":"16:00"},{"day":7,"from":"08:00","to":"16:00"}]');
+            $table->json('every_day_times')->default('{"from":"08:00","to":"16:00"}');
+            $table->json('weekdays_times')->default('[{"day":6,"from":"08:00","to":"16:00"},{"day":7,"from":"08:00","to":"16:00"}]');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
