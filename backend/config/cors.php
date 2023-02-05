3b105d3b05a02d6d954323542ae2e20a474d5776
Pavlo <pavel31.pavel31@gmail.com>
2023-02-05 22:39:07 +0200

    try connect origin

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,25 +15,17 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'https://track.local',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        'Access-Control-Allow-Origin',
-        'Authorization'
-    ],
+    'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ["*"],
 
     'max_age' => 0,
 
